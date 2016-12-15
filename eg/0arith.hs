4685b79421fb6793a0317394bd455d076f8cf700
Rudy Matela <rudy@matela.com.br>
2016-12-15 11:08:59 +0000

    eg/0arith: remove uneeded $'s

--- eg/0arith.hs
+++ eg/0arith.hs
@@ -5,8 +5,8 @@ main = speculate args
   { atoms =
       [ showConstant (0::Int)
       , showConstant (1::Int)
-      , constant "id" $ (id :: Int -> Int)
-      , constant "+"  $ ((+) :: Int -> Int -> Int)
-      , constant "*"  $ ((*) :: Int -> Int -> Int)
+      , constant "id" (id :: Int -> Int)
+      , constant "+"  ((+) :: Int -> Int -> Int)
+      , constant "*"  ((*) :: Int -> Int -> Int)
       ]
   }
