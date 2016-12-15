143710a6b98cc507a94ecd25eb7f80ae160d660d
Rudy Matela <rudy@matela.com.br>
2016-12-15 11:53:22 +0000

    eg/numbool: reorder atoms

--- eg/numbool.hs
+++ eg/numbool.hs
@@ -4,18 +4,18 @@ import Test.LeanCheck ((==>))
 main :: IO ()
 main = speculate args
   { atoms =
-      [ showConstant False
+      [ showConstant (0::Int)
+      , showConstant (1::Int)
+      , constant "+"   ((+)  :: Int -> Int -> Int)
+      , constant "abs" (abs  :: Int -> Int)
+
+      , constant "<="  ((<=) :: Int -> Int -> Bool)
+      , constant "<"   ((<)  :: Int -> Int -> Bool)
+      , constant "=="  ((==) :: Int -> Int -> Bool)
+      , showConstant False
       , showConstant True
       , constant "not" not
     --, constant "&&"  (&&)
     --, constant "||"  (||)
-
-      , showConstant (0::Int)
-      , showConstant (1::Int)
-      , constant "+"   ((+)  :: Int -> Int -> Int)
-      , constant "<="  ((<=) :: Int -> Int -> Bool)
-      , constant "<"   ((<)  :: Int -> Int -> Bool)
-      , constant "=="  ((==) :: Int -> Int -> Bool)
-      , constant "abs" (abs  :: Int -> Int)
       ]
   }
