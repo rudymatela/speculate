3c69e0628748722079203a8a78ea73a95c26751a
Rudy Matela <rudy@matela.com.br>
2016-12-16 15:45:30 +0000

    oddeven: comment explaining strange behaviour
    when using conditionAtoms and equationAtoms

--- eg/oddeven.hs
+++ eg/oddeven.hs
@@ -14,4 +14,20 @@ main = speculate args
       , constant "`mod`"  $ mod  -:> int
       , constant "=="     $ (==) -:> int
       ]
+{- after adding the following, conditions do not appear but in a way should:
+  , conditionAtoms =
+      [ constant "`mod`"  $ mod  -:> int
+      , constant "=="     $ (==) -:> int
+      ]
+  , equationAtoms =
+      [ constant "odd"    $ odd  -:> int
+      , constant "even"   $ even -:> int
+      ]
+-- the problem is that "odd" and "even" are actually smaller versions of the
+-- preconditions we "want":
+-- > (x `mod` 2 == 0) == even x
+-- conditionAtoms and equationAtoms should be used with care!
+-- TODO: add that somewhere in the docs
+-- TODO: make this not happen?  would be really hard: theorize twice!
+-}
   }
