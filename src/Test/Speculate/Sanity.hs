3a2737ef6449fe0caf82b583d24a34aa10e39458
Rudy Matela <rudy@matela.com.br>
2017-02-14 15:50:58 +0000

    Improve sanity

--- src/Test/Speculate/Sanity.hs
+++ src/Test/Speculate/Sanity.hs
@@ -10,6 +10,7 @@ import Test.Speculate.Expr
 import Test.LeanCheck ((==>))
 import Data.Maybe (fromMaybe)
 import Data.List (intercalate)
+import Test.Speculate.Utils
 
 (-==>-) :: Expr -> Expr -> Expr
 e1 -==>- e2 = impliesE :$ e1 :$ e2 where impliesE = constant "==>" (==>)
@@ -22,11 +23,11 @@ infixr 0 -&&-
 -- returns a list of errors on the Eq instances (if any)
 -- returns an empty list when ok
 eqErrors :: Instances -> TypeRep -> Int -> [String]
-eqErrors is t n = ["not reflexive"  | tru $  x -==- x]
-               ++ ["not symmetric"  | tru $ (x -==- y) -==- (y -==- x)]
-               ++ ["not transitive" | tru $  x -==- y -&&- y -==- z -==>- x -==- z]
+eqErrors is t n = ["not reflexive"  | f $  x -==- x]
+               ++ ["not symmetric"  | f $ (x -==- y) -==- (y -==- x)]
+               ++ ["not transitive" | f $  x -==- y -&&- y -==- z -==>- x -==- z]
   where
-  tru = true is n
+  f = false is n
   e1 -==- e2 = fromMaybe falseE $ equation is e1 e2
   x = Var "x" t
   y = Var "y" t
@@ -34,11 +35,11 @@ eqErrors is t n = ["not reflexive"  | tru $  x -==- x]
 
 -- returns a list of errors on the Ord instance (if any)
 ordErrors :: Instances -> TypeRep -> Int -> [String]
-ordErrors is t n = ["not reflexive"     | tru $ x -<=- x]
-                ++ ["not antisymmetric" | tru $ x -<=- y -&&- y -<=- x -==>- x -==- y]
-                ++ ["not transitive"    | tru $ x -<=- y -&&- y -<=- z -==>- x -<=- z]
+ordErrors is t n = ["not reflexive"     | f $ x -<=- x]
+                ++ ["not antisymmetric" | f $ x -<=- y -&&- y -<=- x -==>- x -==- y]
+                ++ ["not transitive"    | f $ x -<=- y -&&- y -<=- z -==>- x -<=- z]
   where
-  tru = true is n
+  f = false is n
   e1 -==- e2 = fromMaybe falseE $ equation is e1 e2
   e1 -<=- e2 = fromMaybe falseE $ comparisonLE is e1 e2
   x = Var "x" t
@@ -47,11 +48,11 @@ ordErrors is t n = ["not reflexive"     | tru $ x -<=- x]
 
 eqOrdErrors :: Instances -> Int -> TypeRep -> [String]
 eqOrdErrors is n t = [ "(==) :: " ++ show t ++ " -> " ++ show t ++ " -> Bool "
-                       ++ "is not an equiavalence (" ++ intercalate ", " es ++ ")"
-                     | let es = eqErrors is t n, not (null es) ]
+                       ++ "is not an equiavalence  (" ++ intercalate ", " es ++ ")"
+                     | let es = eqErrors is t n, isEq is t, not (null es) ]
                   ++ [ "(<=) :: " ++ show t ++ " -> " ++ show t ++ " -> Bool "
-                       ++ "is not an ordering (" ++ intercalate ", " es ++ ")"
-                     | let es = ordErrors is t n, not (null es) ]
+                       ++ "is not an ordering  (" ++ intercalate ", " es ++ ")"
+                     | let es = ordErrors is t n, isOrd is t, not (null es) ]
 
-instanceErrors :: Instances -> Int -> [String]
-instanceErrors is n = undefined
+instanceErrors :: Instances -> Int -> [TypeRep] -> [String]
+instanceErrors is n = concatMap $ eqOrdErrors is n
