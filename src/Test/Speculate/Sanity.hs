3fb2bb0dc496378e3d7ee7a44a198e42bdb0e3c9
Rudy Matela <rudy@matela.com.br>
2017-02-14 17:41:19 +0000

    Sanity: swap arguments of some functions

--- src/Test/Speculate/Sanity.hs
+++ src/Test/Speculate/Sanity.hs
@@ -22,8 +22,8 @@ infixr 3 -&&-
 
 -- returns a list of errors on the Eq instances (if any)
 -- returns an empty list when ok
-eqErrors :: Instances -> TypeRep -> Int -> [String]
-eqErrors is t n =
+eqErrors :: Instances -> Int -> TypeRep -> [String]
+eqErrors is n t =
      ["not reflexive"  | f   (x -==- x)]
   ++ ["not symmetric"  | f  ((x -==- y) -==- (y -==- x))]
   ++ ["not transitive" | f (((x -==- y) -&&- (y -==- z)) -==>- (x -==- z))]
@@ -35,14 +35,14 @@ eqErrors is t n =
   z = Var "z" t
 
 -- returns a list of errors on the Ord instance (if any)
-ordErrors :: Instances -> TypeRep -> Int -> [String]
-ordErrors is t n =
+ordErrors :: Instances -> Int -> TypeRep -> [String]
+ordErrors is n t =
      ["not reflexive"     | f   (x -<=- x)]
   ++ ["not antisymmetric" | f (((x -<=- y) -&&- (y -<=- x)) -==>- (x -==- y))]
   ++ ["not transitive"    | f (((x -<=- y) -&&- (y -<=- z)) -==>- (x -<=- z))]
   where
   f = false is n
-  e1 -==- e2 = fromMaybe falseE $ equation is e1 e2
+  e1 -==- e2 = fromMaybe falseE $ equation     is e1 e2
   e1 -<=- e2 = fromMaybe falseE $ comparisonLE is e1 e2
   x = Var "x" t
   y = Var "y" t
@@ -51,16 +51,16 @@ ordErrors is t n =
 eqOrdErrors :: Instances -> Int -> TypeRep -> [String]
 eqOrdErrors is n t =
      [ "(==) :: " ++ ty ++ "  is not an equiavalence (" ++ intercalate ", " es ++ ")"
-     | let es = eqErrors is t n, isEq is t, not (null es) ]
+     | let es = eqErrors is n t, isEq is t, not (null es) ]
   ++ [ "(<=) :: " ++ ty ++ "  is not an ordering ("     ++ intercalate ", " es ++ ")"
-     | let es = ordErrors is t n, isOrd is t, not (null es) ]
+     | let es = ordErrors is n t, isOrd is t, not (null es) ]
   ++ [ "(==) and (<=) :: " ++ ty ++ " are inconsistent: (x == y) /= (x <= y && y <= x)"
      | false is n $ (x -==- y) -==- (x -<=- y -&&- y -<=- x)]
   where
   x = Var "x" t
   y = Var "y" t
   z = Var "z" t
-  e1 -==- e2 = fromMaybe falseE $ equation is e1 e2
+  e1 -==- e2 = fromMaybe falseE $ equation     is e1 e2
   e1 -<=- e2 = fromMaybe falseE $ comparisonLE is e1 e2
   ty = show t ++ " -> " ++ show t ++ " -> Bool"
 
