3dff30a4c28a59ab28a89b4291fe0bee7a5565b5
Rudy Matela <rudy@matela.com.br>
2017-02-14 16:49:54 +0000

    Sanity: reparethize

--- src/Test/Speculate/Sanity.hs
+++ src/Test/Speculate/Sanity.hs
@@ -14,18 +14,19 @@ import Test.Speculate.Utils
 
 (-==>-) :: Expr -> Expr -> Expr
 e1 -==>- e2 = impliesE :$ e1 :$ e2 where impliesE = constant "==>" (==>)
-infixr 0 -==>-
+infixr 1 -==>-
 
 (-&&-) :: Expr -> Expr -> Expr
 e1 -&&- e2 = andE :$ e1 :$ e2 where andE = constant "&&" (&&)
-infixr 0 -&&-
+infixr 3 -&&-
 
 -- returns a list of errors on the Eq instances (if any)
 -- returns an empty list when ok
 eqErrors :: Instances -> TypeRep -> Int -> [String]
-eqErrors is t n = ["not reflexive"  | f $  x -==- x]
-               ++ ["not symmetric"  | f $ (x -==- y) -==- (y -==- x)]
-               ++ ["not transitive" | f $  x -==- y -&&- y -==- z -==>- x -==- z]
+eqErrors is t n =
+     ["not reflexive"  | f   (x -==- x)]
+  ++ ["not symmetric"  | f  ((x -==- y) -==- (y -==- x))]
+  ++ ["not transitive" | f (((x -==- y) -&&- (y -==- z)) -==>- (x -==- z))]
   where
   f = false is n
   e1 -==- e2 = fromMaybe falseE $ equation is e1 e2
@@ -35,9 +36,10 @@ eqErrors is t n = ["not reflexive"  | f $  x -==- x]
 
 -- returns a list of errors on the Ord instance (if any)
 ordErrors :: Instances -> TypeRep -> Int -> [String]
-ordErrors is t n = ["not reflexive"     | f $ x -<=- x]
-                ++ ["not antisymmetric" | f $ x -<=- y -&&- y -<=- x -==>- x -==- y]
-                ++ ["not transitive"    | f $ x -<=- y -&&- y -<=- z -==>- x -<=- z]
+ordErrors is t n =
+     ["not reflexive"     | f   (x -<=- x)]
+  ++ ["not antisymmetric" | f (((x -<=- y) -&&- (y -<=- x)) -==>- (x -==- y))]
+  ++ ["not transitive"    | f (((x -<=- y) -&&- (y -<=- z)) -==>- (x -<=- z))]
   where
   f = false is n
   e1 -==- e2 = fromMaybe falseE $ equation is e1 e2
@@ -50,7 +52,7 @@ eqOrdErrors :: Instances -> Int -> TypeRep -> [String]
 eqOrdErrors is n t =
      [ "(==) :: " ++ ty ++ "  is not an equiavalence (" ++ intercalate ", " es ++ ")"
      | let es = eqErrors is t n, isEq is t, not (null es) ]
-  ++ [ "(<=) :: " ++ ty ++ "  is not an ordering (" ++ intercalate ", " es ++ ")"
+  ++ [ "(<=) :: " ++ ty ++ "  is not an ordering ("     ++ intercalate ", " es ++ ")"
      | let es = ordErrors is t n, isOrd is t, not (null es) ]
   ++ [ "(==) and (<=) :: " ++ ty ++ " are inconsistent: (x == y) /= (x <= y && x >= y)"
      | false is n $ (x -==- y) -==- (x -<=- y -&&- y -<=- x)]
