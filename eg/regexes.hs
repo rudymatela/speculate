59a732b53f548a213c573be12507862e262ca7b9
Rudy Matela <rudy@matela.com.br>
2017-02-15 17:40:30 +0000

    faster regexes

--- eg/regexes.hs
+++ eg/regexes.hs
@@ -1,6 +1,9 @@
+{-# LANGUAGE StandaloneDeriving #-}
 import Test.Speculate hiding (match)
 import Data.Function (on)
 import Regex
+import Data.Maybe (fromJust, fromMaybe)
+import qualified Data.Map as M
 
 instance Listable Symbol where
   tiers = cons0 (Symbol 'a')
@@ -15,25 +18,32 @@ instance Listable a => Listable (RE a) where
        \/ cons2 (:+)
        \/ cons2 (:.)
 
+deriving instance Eq a => Eq (RE a)
+deriving instance Ord a => Ord (RE a)
+
 class    Charable a      where toChar :: a -> Char
 instance Charable Char   where toChar = id
 instance Charable Symbol where toChar (Symbol c) = c
 
-testMatches :: (Listable a, Charable a) => RE a -> [Bool]
-testMatches r = map (\e -> match toChar e r) $ take 25 list
+testMatches :: (Listable a, Show a, Charable a) => RE a -> [Bool]
+testMatches r = map (\e -> match toChar e r) $ take 100 list
 
-instance (Listable a, Show a, Charable a) => Eq (RE a) where
-  (==) = (==) `on` testMatches
+testMatchesMemory :: (Listable a, Show a, Charable a, Ord a) => M.Map (RE a) [Bool]
+testMatchesMemory = foldr (uncurry M.insert) M.empty $ take 1080 $ map rm list
+  where
+  rm r = (r, testMatches r)
 
-instance (Listable a, Show a, Charable a) => Ord (RE a) where
-  compare = compare `on` testMatches
+memoTestMatches :: (Listable a, Show a, Charable a, Ord a) => RE a -> [Bool]
+memoTestMatches r = fromMaybe (testMatches r) (M.lookup r testMatchesMemory)
 
 main :: IO ()
 main = speculate args
   { maxTests = 25
   , maxSize = 4
   , instances =
-      [ ins "c" (undefined :: Symbol)
+      [ eqWith  $ ((==) `on` memoTestMatches :: RE Symbol -> RE Symbol -> Bool)
+      , ordWith $ ((<=) `on` memoTestMatches :: RE Symbol -> RE Symbol -> Bool)
+      , ins "c" (undefined :: Symbol)
       , ins "r" (undefined :: RE Symbol)
       ]
   , constants =
