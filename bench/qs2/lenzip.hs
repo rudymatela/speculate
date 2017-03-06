00a6da40afe9352b17c937c2f7468cc5ee33a8da
Rudy Matela <rudy@matela.com.br>
2017-03-06 16:13:13 +0000

    bench/qs2/lenzip: use predicateGen, no effect...

--- bench/qs2/lenzip.hs
+++ bench/qs2/lenzip.hs
@@ -2,8 +2,8 @@ import QuickSpec hiding (insert)
 import Data.List (sort, insert)
 import Test.QuickCheck
 import Data.Dynamic
+import Control.Monad
 
-main :: IO ()
 main =
   quickSpec signature
     { maxTermSize = Just 5
@@ -14,6 +14,14 @@ main =
         , constant "zip"   (zip :: [Int] -> [Int] -> [(Int,Int)])
         ]
     , predicates =
-        [ predicate "eqLen" ((\xs ys -> length xs == length ys) :: [Int] -> [Int] -> Bool)
+--      [ predicate "eqLen" ((\xs ys -> length xs == length ys) :: [Int] -> [Int] -> Bool)
+        [ predicateGen "eqLen" ((\xs ys -> length xs == length ys) :: [Int] -> [Int] -> Bool) eqLenGen
         ]
     }
+
+eqLenGen :: Gen [Dynamic]
+eqLenGen = do
+  len <- arbitrary
+  xs1 <- (replicateM len arbitrary :: Gen [Int])
+  xs2 <- (replicateM len arbitrary :: Gen [Int])
+  return [toDyn xs1, toDyn xs2]
