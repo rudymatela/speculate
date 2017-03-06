177e25146ced0aef7b5d52865e84c7248a76ebea
Rudy Matela <rudy@matela.com.br>
2017-03-06 14:51:36 +0000

    eg/length: re-order signature

--- eg/length.hs
+++ eg/length.hs
@@ -4,13 +4,16 @@ import Data.List (isSubsequenceOf)
 main :: IO ()
 main = speculate args
   { constants =
-      [ constant "length" $ length  -:> [int]
-      , background
+      [ background
       , showConstant ([] :: [Int])
       , constant ":"      $ (:)     -:>  int
       , constant "++"     $ (++)    -:> [int]
       , showConstant (0 :: Int)
 --    , showConstant (1 :: Int)
+--    , constant "==" $ (==) -:> int
+      , foreground
+      , constant "length" $ length  -:> [int]
+--    , constant "zip"    $ zip  -:> [int] ->:> [int]
       ]
   , instances = [ ordWith (isSubsequenceOf :: [Int] -> [Int] -> Bool) ]
   }
