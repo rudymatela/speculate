7d66c9e5d53f5321ccec030d58a44a45f7b91a46
Rudy Matela <rudy@matela.com.br>
2017-03-10 11:41:30 +0000

    Further modularize Pretty printing module

--- src/Test/Speculate/Pretty.hs
+++ src/Test/Speculate/Pretty.hs
@@ -1,8 +1,8 @@
 -- | Pretty printing of Equations, Inequalities and Conditional Equations
 module Test.Speculate.Pretty
-  ( prettyThy
-  , prettyShy
-  , prettyChy
+  ( prettyThy, prettyEquations
+  , prettyShy, prettySemiEquations
+  , prettyChy, prettyCondEquations
   )
 where
 
@@ -16,24 +16,34 @@ type Equation = (Expr,Expr)
 type CondEquation = (Expr,Expr,Expr)
 
 prettyThy :: (Equation -> Bool) -> Instances -> Thy -> String
-prettyThy shouldShow ti =
-  table "r l l" . map showEquation . finalEquations shouldShow ti
+prettyThy shouldShow ti = prettyEquations . finalEquations shouldShow ti
+
+prettyChy :: (CondEquation -> Bool) -> Chy -> String
+prettyChy shouldShow = prettyCondEquations . finalCondEquations shouldShow
+
+prettyShy :: (Equation -> Bool) -> Instances -> (Expr -> Expr -> Bool) -> Shy -> String
+prettyShy shouldShow insts equivalentInstanceOf =
+  prettySemiEquations . finalSemiEquations shouldShow insts equivalentInstanceOf
+
+prettyEquations :: [Equation] -> String
+prettyEquations =
+  table "r l l" . map showEquation
   where
   showEquation (e1,e2)
 --  | typ e1 == boolTy = [showOpExpr "<==>" e1, "<==>", showOpExpr "<==>" e2]
     | otherwise        = [showOpExpr "==" e1, "==", showOpExpr "==" e2]
 
-prettyChy :: (CondEquation -> Bool) -> Chy -> String
-prettyChy shouldShow =
-    table "r r r l l"
-  . map (\(pre,e1,e2) -> [ showOpExpr "==>" pre
-                         , "==>", showOpExpr "==" e1
-                         , "==",  showOpExpr "==" e2 ])
-  . finalCondEquations shouldShow
-
-prettyShy :: (Equation -> Bool) -> Instances -> (Expr -> Expr -> Bool) -> Shy -> String
-prettyShy shouldShow insts equivalentInstanceOf =
-  table "r l l" . map showSELine . finalSemiEquations shouldShow insts equivalentInstanceOf
+prettySemiEquations :: [Equation] -> String
+prettySemiEquations =
+  table "r l l" . map showSELine
   where
   showSELine (e1,e2) = showLineWithOp (if typ e1 == boolTy then "==>" else "<=") (e1,e2)
   showLineWithOp o (e1,e2) = [showOpExpr o e1, o, showOpExpr o e2]
+
+prettyCondEquations :: [CondEquation] -> String
+prettyCondEquations =
+  table "r r r l l" . map showCELine
+  where
+  showCELine (ce,e1,e2) = [ showOpExpr "==>" ce
+                          , "==>", showOpExpr "==" e1
+                          , "==",  showOpExpr "==" e2 ]
