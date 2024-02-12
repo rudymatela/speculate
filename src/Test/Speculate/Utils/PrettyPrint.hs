fb132daea543681b8a4a5cffdf7a039de18eba30
Rudy Matela <rudy@matela.com.br>
2024-02-12 14:48:29 +0100

    improve PrettyPrint

--- src/Test/Speculate/Utils/PrettyPrint.hs
+++ src/Test/Speculate/Utils/PrettyPrint.hs
@@ -14,7 +14,6 @@ module Test.Speculate.Utils.PrettyPrint
   , spaces
   )
 where
--- TODO: Fix somewhat inefficient implementations, i.e.: heavy use of '(++)'.
 
 import Data.List (transpose)
 import Data.Char (isSpace)
@@ -27,12 +26,13 @@ import Test.LeanCheck ((+|))
 -- >  "asdfzxvc\n\
 -- >  \qw  as\n"
 beside :: String -> String -> String
-beside cs ds = unlines $ zipWith (++) (normalize ' ' css) dss
-  where [css,dss] = normalize "" [lines cs,lines ds]
+beside cs ds  =  unlines $ zipWith (++) (normalize ' ' css) dss
+  where
+  [css,dss]  =  normalize "" [lines cs,lines ds]
 
 -- | Append two Strings on top of each other, adding line breaks *when needed*.
 above :: String -> String -> String
-above cs ds = if last cs == '\n' || head ds == '\n'
+above cs ds  =  if last cs == '\n' || head ds == '\n'
                 then cs ++ ds
                 else cs ++ '\n':ds
 
@@ -65,15 +65,15 @@ above cs ds = if last cs == '\n' || head ds == '\n'
 -- >   \ 123   456  3\n\
 -- >   \       789\n"
 table :: String -> [[String]] -> String
-table s []  = ""
-table s sss = unlines
-            . map (removeTrailing ' ' . concat . (+| spaces s))
-            . transpose
-            . zipWith (`normalizeTo` ' ') (discard  isSpace s)
-            . foldr1 (zipWith (++))
-            . map (normalize "" . map lines)
-            . normalize ""
-            $ sss
+table s []   =  ""
+table s sss  =  unlines
+             .  map (removeTrailing ' ' . concat . (+| spaces s))
+             .  transpose
+             .  zipWith (`normalizeTo` ' ') (discard  isSpace s)
+             .  foldr1 (zipWith (++))
+             .  map (normalize "" . map lines)
+             .  normalize ""
+             $  sss
 
 -- | Fits a list to a certain width by appending a certain value
 --
@@ -81,39 +81,39 @@ table s sss = unlines
 --
 -- > fit 0 6 [1,2,3] == [1,2,3,0,0,0]
 fit :: a -> Int -> [a] -> [a]
-fit x n xs = xs ++ replicate (n - length xs) x
+fit x n xs  =  xs ++ replicate (n - length xs) x
 
 fitR :: a -> Int -> [a] -> [a]
-fitR x n xs = replicate (n - length xs) x ++ xs
+fitR x n xs  =  replicate (n - length xs) x ++ xs
 
 -- | normalize makes all list the same length by adding a value
 --
 -- > normalize ["asdf","qw","er"] == normalize ["asdf","qw  ","er  "]
 normalize :: a -> [[a]] -> [[a]]
-normalize x xs = map (x `fit` maxLength xs) xs
+normalize x xs  =  map (x `fit` maxLength xs) xs
 
 normalizeR :: a -> [[a]] -> [[a]]
-normalizeR x xs = map (x `fitR` maxLength xs) xs
+normalizeR x xs  =  map (x `fitR` maxLength xs) xs
 
 normalizeTo :: Char -> a -> [[a]] -> [[a]]
-normalizeTo 'l' = normalize
-normalizeTo 'r' = normalizeR
-normalizeTo _   = error "normalizeTo: unhandled case"
+normalizeTo 'l'  =  normalize
+normalizeTo 'r'  =  normalizeR
+normalizeTo _    =  error "normalizeTo: unhandled case"
 
 -- | Given a list of lists returns the maximum length
 maxLength :: [[a]] -> Int
-maxLength = maximum . (0:) . map length
+maxLength  =  maximum . (0:) . map length
 
 removeTrailing :: Eq a => a -> [a] -> [a]
-removeTrailing x = reverse
-                 . dropWhile (==x)
-                 . reverse
+removeTrailing x  =  reverse
+                  .  dropWhile (==x)
+                  .  reverse
 
 spaces :: String -> [String]
-spaces "" = []
-spaces s = case takeWhile isSpace s of
+spaces ""  =  []
+spaces s  =  case takeWhile isSpace s of
              "" ->      spaces (dropWhile isntSpace $ dropWhile isSpace s)
              s' -> s' : spaces (dropWhile isntSpace $ dropWhile isSpace s)
 
 isntSpace :: Char -> Bool
-isntSpace = not . isSpace
+isntSpace  =  not . isSpace
